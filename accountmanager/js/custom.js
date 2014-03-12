ae4473d59cb210e2b20a5a8417a3b028cd2421d7
Michael Schmid <michael@amazeelabs.com>
2014-03-12 23:54:54 +0100

    more updates

--- accountmanager/js/custom.js
+++ accountmanager/js/custom.js
@@ -3,4 +3,12 @@
 
   $('a.pop').popover();
 
+	$('.page-scroll a, a.page-scroll').bind('click', function(event) {
+	    var $anchor = $(this);
+	    $('html, body').stop().animate({
+	        scrollTop: $($anchor.attr('href')).offset().top
+	    }, 1500, 'easeInOutExpo');
+	    event.preventDefault();
+	});
+
 }(jQuery);
