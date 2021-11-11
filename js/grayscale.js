28a50c155302817db2af8291d5117db918b45c44
Ruthie <ruthhfuller@gmail.com>
2021-11-11 12:11:36 -0500

    update map to va

--- js/grayscale.js
+++ js/grayscale.js
@@ -21,7 +21,7 @@ $(function() {
 //Google Map Skin - Get more at http://snazzymaps.com/
 var myOptions = {
     zoom: 13,
-    center: new google.maps.LatLng(30.265588, -97.745684),
+    center: new google.maps.LatLng(37.669431383294004, -76.44034388257226),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     disableDefaultUI: true,
     styles: [{
